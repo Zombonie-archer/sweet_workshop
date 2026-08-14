63226990015ebb31b076074a49241cdc4074db67
yaroslavmakson@gmail.com <yaroslavmakson@gmail.com>
2026-08-14 09:11:47 +0300

    add default params

--- src/js/axios.js
+++ src/js/axios.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const BASE_URL = 'https://deserts-store.b.goit.study/api/';
 
-export default async function request({ method, body, route }) {
+export default async function request({ method = "get", body, route }) {
   const url = BASE_URL + route;
-  if (method === 'get') {
+  if (method.toLowerCase() === 'get') {
     return await axios.get(url, {
       params: body,
     });
-  } else if (method === 'post') {
+  } else if (method.toLowerCase() === 'post') {
     return await axios.post(url, body);
   }
 }
