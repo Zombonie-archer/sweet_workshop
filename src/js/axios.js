f5309dbe8a5ba610a5d1ee48a94c45832fb5e513
yaroslavmakson@gmail.com <yaroslavmakson@gmail.com>
2026-08-15 20:53:24 +0300

    add loader

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
+import { showLoader, hideLoader } from './utils.js';
 
 const BASE_URL = 'https://deserts-store.b.goit.study/api/';
-const loader = document.querySelector('.global-loader-overlay');
 
 export default async function request({ method = "get", body = {}, route = "" }) {
-  loader.classList.remove('hidden');
+  showLoader();
   const url = BASE_URL + route;
   let response = {};
   if (method.toLowerCase() === 'get') {
@@ -14,6 +14,7 @@ export default async function request({ method = "get", body = {}, route = "" })
   } else if (method.toLowerCase() === 'post') {
     response = await axios.post(url, body);
   }
-  loader.classList.add('hidden');
+  
+  hideLoader();
   return response;
 }
