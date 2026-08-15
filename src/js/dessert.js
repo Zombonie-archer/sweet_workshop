9215b5dc95bc87d3d084b7db11b870a3219be24d
Maxim <ratutuy55@gmail.com>
2026-08-15 05:05:57 +0300

    add load more button functional

--- src/js/dessert.js
+++ src/js/dessert.js
@@ -1,7 +1,10 @@
 import request from './axios.js';
+import iziToast from 'izitoast';
+import "izitoast/dist/css/iziToast.min.css";
 
 const filterContainer = document.querySelector('.dessert-filter');
 const dessertContainer = document.querySelector('.dessert-list');
+const loadMore = document.querySelector('.load-more');
 let currentPage = 1;
 
 filterContainer.innerHTML = `
@@ -11,23 +14,16 @@ filterContainer.innerHTML = `
     </label>
 `;
 
-async function dessertMarcup() {
+async function categoryMarcup() {
     try {
         const filters = await request({ method: 'get', route: 'categories' });
         const categories = filters.data;
 
-        const desserts = await request({ method: 'get', body: { page: currentPage, limit: 8 }, route: 'desserts' });
-        const dessertsCard = desserts.data.desserts;
-        console.log(dessertsCard);
-        
         filterContainer.insertAdjacentHTML('beforeend', createCategory(categories))
-        dessertContainer.insertAdjacentHTML('beforeend', createDesserts(dessertsCard))
     } catch (error) {
-        console.log(error.message);
-        
+        console.log("Error category load", error.message);
     }
 };
-
 function createCategory(categories) {
     return categories.map(({ name }) => `
         <label class="dessert-category-label">
@@ -36,6 +32,37 @@ function createCategory(categories) {
         </label>
     
     `).join("");
+};
+categoryMarcup();
+
+async function dessertMarcup() {
+    try {
+        const desserts = await request({ method: 'get', body: { page: currentPage, limit: 8 }, route: 'desserts' });
+        const dessertsCard = desserts.data.desserts;
+        
+        dessertContainer.insertAdjacentHTML('beforeend', createDesserts(dessertsCard));
+       
+        const dessertsCount = dessertContainer.children.length;
+        const totalItems = desserts.data.totalItems;
+
+        if (dessertsCount >= totalItems || dessertsCard.length < 8) {
+            loadMore.style.display = "none";
+            iziToast.warning({
+                title: 'Увага',
+                titleSize: '16',
+                message: 'Ви завантажили всі тістечка',
+                messageSize: '16',
+                position: 'topRight',
+                timeout: 5000
+            })
+        }else {
+            loadMore.style.display = "block";
+        }
+    } catch (error) {
+        console.log(error.message);
+        loadMore.style.display = "block";
+        
+    }
 };
 
 function createDesserts(dessertsCard) {
@@ -47,7 +74,7 @@ function createDesserts(dessertsCard) {
             <p class="crad-descr"> ${description}</p>
             <div class="card-footer">
                 <p class="card-price"><span class="price-value">${price}</span>грн</p>
-                <button class="card-btn" type="button">кнопка</button>
+                <button class="card-btn" type="button"></button>
             </div>
             
         </li>
@@ -56,4 +83,11 @@ function createDesserts(dessertsCard) {
 }
 
 dessertMarcup();
+
+loadMore.addEventListener('click', () => {
+    currentPage += 1;
+    loadMore.style.display = "none";
+    dessertMarcup();
+
+})
 
