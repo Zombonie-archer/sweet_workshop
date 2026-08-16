73ba32d2e044c65e24d27f970c2ee7bd1154591f
leontievasophia <leontievasophia@gmail.com>
2026-08-16 15:35:54 +0300

    Add about us section structure and swiper

--- src/js/aboutUs.js
+++ src/js/aboutUs.js
@@ -4,20 +4,36 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-if (window.innerWidth >= 768){
-const swiper = new Swiper('.aboutSwiper', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+let swiper = null;
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+function initSwiper() {
+  if (window.innerWidth >= 768 && !swiper) {
+    swiper = new Swiper('.aboutSwiper', {
+      modules: [Navigation, Pagination],
+
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+
+  if (window.innerWidth < 768 && swiper) {
+    swiper.destroy(true, true);
+    swiper = null;
+  }
 }
+
+initSwiper();
+
+window.addEventListener('resize', initSwiper);
