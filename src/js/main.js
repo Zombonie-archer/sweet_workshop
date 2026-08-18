5538f7e1909d93ab8d193317953b403e6250c259
yaroslavmakson@gmail.com <yaroslavmakson@gmail.com>
2026-08-18 14:59:54 +0300

    fix productivity

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import './headerModal.js';
+import './hero.js';
+import './dessert.js';
+import './aboutUs.js';
+import './faq.js';
+import './feedback.js';
+import './dessertModal.js';
+import './orderModal.js';
